aa2a4012f68f3f2863df7f35f4d5dce4938eea90
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-11-07 11:43:27 +0100

    leviation end

--- src/js/main.js
+++ src/js/main.js
@@ -110,3 +110,19 @@ document.addEventListener("DOMContentLoaded", function () {
       0
     );
 });
+
+// cursor
+const cursor = document.querySelector(".cursor");
+const links = document.querySelectorAll("a");
+
+document.addEventListener("mousemove", (event) => {
+  cursor.style.top = event.y + "px";
+  cursor.style.left = event.x + "px";
+});
+
+links.forEach((link) => {
+  link.addEventListener("mouseenter", () => cursor.classList.add("link-hover"));
+  link.addEventListener("mouseleave", () =>
+    cursor.classList.remove("link-hover")
+  );
+});
